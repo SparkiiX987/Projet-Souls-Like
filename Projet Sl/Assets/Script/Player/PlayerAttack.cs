4d0daf7d442d6c8e5e5b1194afe8a0abd632ad64
NATEEEUUUH <nateeeuuuh@gmail.com>
2024-01-08 20:38:09 +0100

    Skybox Buggé Trop moche sah c'est trop cool

---
+++ Projet Sl/Assets/Script/Player/PlayerAttack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public InputAction attackAction;
+    public InputAction blockAction;
+
+    public int playerDamage = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
